807dacce85e673d529d6c8ad4abf8456225a3ef9
Arunchellasp <you@example.com>
2024-11-04 20:06:08 +0530

    Load File and Read Header

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-int main(void){
-    printf("Basic C programming: Cryptography\n");
-    
-    // TODO: Load file to memory
-    
-    // TODO: Decrypt all the blocks
-    
-    // TODO: Verify SDBM hash (zero initialized) of all the decrypted blocks
-    
-    // TODO: Sort decrypted message
-    
-    // TODO: Print custom message - text of all decrypted blocks after sorting
-    
+int main() {
+    // Open "data.bin" file in binary read mode
+    FILE *file = fopen("data.bin", "rb");
+    if (!file) {
+        printf("Failed to open file.\n");
+        return 1;
+    }
+
+    // Read the first 4 bytes as the hash
+    uint32_t file_hash;
+    fread(&file_hash, sizeof(uint32_t), 1, file);
+    printf("Read SDBM hash: 0x%08X\n", file_hash);
+
+    // Close the file (we will reopen it in the next step when processing data)
+    fclose(file);
+    return 0;
 }
