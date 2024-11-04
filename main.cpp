2e8c37e061f83aa0a74ee691f679ee0ac036c66c
Arunchellasp <you@example.com>
2024-11-04 19:54:00 +0530

    commit_1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+    printf("Basic C programming: Cryptography\n");
+    
+    // TODO: Load file to memory
+    
+    // TODO: Decrypt all the blocks
+    
+    // TODO: Verify SDBM hash (zero initialized) of all the decrypted blocks
+    
+    // TODO: Sort decrypted message
+    
+    // TODO: Print custom message - text of all decrypted blocks after sorting
+    
+}
